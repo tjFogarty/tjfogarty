9f6ec81e857c26d4eaac9abb096c007fd8196f92
T.J. Fogarty <fogarty.tj@gmail.com>
2013-08-09 22:20:08 +0100

    Updated with some sensible defaults and an index file with some details

--- index.php
+++ index.php
@@ -1,9 +1,28 @@
 <?php
-  $title = "SEO Title";
-  $description = "SEO Description";
+  $title = "Project Scaffold | New Project";
+  $description = "Description";
   $page = "index";
 
-  include('/includes/header.php');
+  include('includes/header.php');
 ?>
 
-<?php include('/includes/footer.php'); ?>
+<header class="grid-container">
+	<div class="grid-100">
+		<h1>Project Scaffold</h1>
+
+		<p>Besides including the header and footer, each page accepts 3 variables declared before calling the header:</p>
+		<ol>
+			<li>
+				<code>$title</code> will be the page title
+			</li>
+			<li>
+				<code>$description</code> will be the description in the meta tag
+			</li>
+			<li>
+				<code>$page</code> is used as a class given to the body tag
+			</li>
+		</ol>
+	</div>
+</header>
+
+<?php include('includes/footer.php'); ?>
