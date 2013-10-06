8d115f45ef546a3db3abd48136a25286e44c978e
T.J. Fogarty <fogarty.tj@gmail.com>
2013-10-06 13:41:07 +0100

    Updated PHP documentation

--- php/functions.php
+++ php/functions.php
@@ -1,4 +1,12 @@
 <?php
+
+/** 
+ * functions.php
+ * @author T.J. Fogarty <fogarty.tj@gmail.com>
+ * @link http://tjfogarty.com
+ */
+
+
 /**
  * Environment
  * Lets us manage assets based on current environment
@@ -6,7 +14,7 @@
 class Environment {
 
 	/**
-	 * @param $localEnvironment - string - default: localhost
+	 * @param $localEnvironment (default value is 'localhost' if nothing is passed)
 	 * @return boolean
 	 */
 	public static function isLocal( $localEnvironment = 'localhost' ) {
